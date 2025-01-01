c7205898aad9990ec6802eb1be1cd47dde881c17
Anton Abdulgalimov <aabdulgalimov@proton.me>
2025-01-01 17:57:13 +0100

    Add request retries

--- todoist_export/core/export.py
+++ todoist_export/core/export.py
@@ -5,10 +5,16 @@ import itertools
 import json
 from dataclasses import asdict
 from pathlib import Path
+from typing import Callable, Any
 
+import requests
 from todoist_api_python.api_async import TodoistAPIAsync
 
 from todoist_export.core.api_token import get_token, set_token
+
+
+REQUEST_MAX_ATTEMPTS = 3
+REQUEST_RETRY_DELAY_SECONDS = 5
 
 
 def export(args: argparse.Namespace) -> None:
@@ -39,19 +45,19 @@ async def _download_data() -> dict:
     api = TodoistAPIAsync(token)
 
     projects, sections, tasks, labels, shared_labels = await asyncio.gather(
-        asyncio.create_task(api.get_projects()),
-        asyncio.create_task(api.get_sections()),
-        asyncio.create_task(api.get_tasks()),
-        asyncio.create_task(api.get_labels()),
-        asyncio.create_task(api.get_shared_labels()),
+        _request(api.get_projects),
+        _request(api.get_sections),
+        _request(api.get_tasks),
+        _request(api.get_labels),
+        _request(api.get_shared_labels),
     )
 
     comments_tasks = itertools.chain(
-        (asyncio.create_task(api.get_comments(project_id=x.id)) for x in projects),
-        (asyncio.create_task(api.get_comments(task_id=x.id)) for x in tasks),
+        (_request(api.get_comments, project_id=x.id) for x in projects),
+        (_request(api.get_comments, task_id=x.id) for x in tasks),
     )
     collaborators_tasks = (
-        asyncio.create_task(api.get_collaborators(x.id)) for x in projects
+        _request(api.get_collaborators, project_id=x.id) for x in projects
     )
     comment_lists, collaborator_lists = await asyncio.gather(
         asyncio.gather(*comments_tasks),
@@ -77,3 +83,14 @@ async def _download_data() -> dict:
         "comments": [asdict(item) for item in comments],
         "collaborators": [asdict(item) for item in collaborators],
     }
+
+
+async def _request(api_call: Callable, *args, **kwargs) -> Any:
+    for attempt in range(1, REQUEST_MAX_ATTEMPTS + 1):
+        try:
+            return await api_call(*args, **kwargs)
+        except requests.exceptions.HTTPError as err:
+            if attempt < REQUEST_MAX_ATTEMPTS and err.response.status_code >= 500:
+                await asyncio.sleep(REQUEST_RETRY_DELAY_SECONDS)
+            else:
+                raise
